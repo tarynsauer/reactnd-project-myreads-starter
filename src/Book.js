77a904b5ad1f0cdd986b7b0bf47d6a22c2104188
Taryn Sauer <taryn.sauer@gmail.com>
2018-07-25 15:55:24 -0500

    refactor: Make Book a Functional Component

--- src/Book.js
+++ src/Book.js
@@ -1,22 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ShelfChanger from './ShelfChanger'
 
-class Book extends Component {
-  render() {
-    const { authors, shelf, title } = this.props.book;
-    const cover = this.props.book.imageLinks.thumbnail;
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${cover}")` }}></div>
-          <ShelfChanger book={this.props.book} currentlySelected={shelf} updateShelf={this.props.updateShelf} />
-        </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{authors}</div>
-      </div>
-    )
-  }
-}
+const Book = ({ book, updateShelf }) => (
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+      <ShelfChanger book={ book } currentlySelected={ book.shelf } updateShelf={ updateShelf } />
+    </div>
+    <div className="book-title">{ book.title }</div>
+    <div className="book-authors">{ book.authors }</div>
+  </div>
+)
 
 export default Book;
