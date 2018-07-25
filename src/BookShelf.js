8dbc1d0d6b652d8df33e6d9aa1c04c206440ca35
Taryn Sauer <taryn.sauer@gmail.com>
2018-07-25 15:59:51 -0500

    refactor: Make BookShelf a Functional Component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import BookList from './BookList'
 
-class BookShelf extends Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.sectionTitle}</h2>
-        <BookList books={this.props.books} moveShelf={this.props.moveShelf} />
-      </div>
-    )
-  }
-}
+const BookShelf = ({ books, sectionTitle, moveShelf }) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{ sectionTitle }</h2>
+    <BookList books={ books } moveShelf={ moveShelf } />
+  </div>
+)
 
 export default BookShelf;
