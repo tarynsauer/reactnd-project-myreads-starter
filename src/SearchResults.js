2e23e4d8e9c84cd75b2d0147c4493948b1f9ec5e
Taryn Sauer <taryn.sauer@gmail.com>
2018-07-25 16:05:48 -0500

    refactor: Make SearchResults a Functional Component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class SearchResults extends Component {
-  render() {
-    return (
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {this.props.searchResults.map((book) => {
-            return (<li key={book.id}>
-              <Book book={book} updateShelf={this.props.updateShelf} />
-            </li>)
-          })}
-        </ol>
-      </div>
-    )
-  }
-}
+const SearchResults = ({ searchResults, updateShelf }) => (
+  <div className="search-books-results">
+    <ol className="books-grid">
+      { searchResults.map(book => {
+        return (<li key={ book.id }>
+          <Book book={ book } updateShelf={ updateShelf } />
+        </li>)
+      })}
+    </ol>
+  </div>
+)
 
 export default SearchResults
