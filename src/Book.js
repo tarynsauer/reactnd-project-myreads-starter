571ca7b06d922bed55642c9c7a55cc286d077c22
Taryn Sauer <taryn.sauer@gmail.com>
2018-07-28 09:04:59 -0500

    fix: Display books without cover images

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,9 @@ import ShelfChanger from './ShelfChanger'
 const Book = ({ book, updateShelf }) => (
   <div className="book">
     <div className="book-top">
-      <div className="book-cover" style={{ backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+      <div className="book-cover"
+        style={{ backgroundImage: book.imageLinks === undefined ? '' : `url("${book.imageLinks.thumbnail}")` }}>
+      </div>
       <ShelfChanger book={ book } currentlySelected={ book.shelf } updateShelf={ updateShelf } />
     </div>
     <div className="book-title">{ book.title }</div>
